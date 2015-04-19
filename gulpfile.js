290c6cdc6bec9a7b5975fbf97cee73d12ae83e5b
Rai Butera <rai@squirtle.local>
2015-04-19 19:26:48 +0100

    installed gulp-util and scaffolded out gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,52 @@
+// inject gulp
 var gulp = require('gulp');
 
-gulp.task('default', function() {
-  // place code for your default task here
-});
+// gulp plugin injection
+var runSequence = require('run-sequence');
+var gulpLoadPlugins = require('gulp-load-plugins');
+var plugins = gulpLoadPlugins();
+
+// individual plugins from gulp-load-plugins
+var jshint = plugins.jshint;
+var uglify = plugins.uglify;
+var rename = plugins.rename;
+var concat = plugins.concat;
+var webpack = plugins.webpack;
+var sourcemaps = plugins.sourcemaps;
+var util = plugins.util;
+var log = util.log;
+var colors = util.colors;
+
+/** GULP TASKS */
+
+/**
+ * default grunt task - transpiles
+ */
+function defaultTask(){
+
+}
+gulp.task('default', defaultTask);
+
+/**
+ * builds @ ./dist/jsnlog-angular.js and minified @ ./dist/jsnlog-angular.min.js
+ */
+function distPrepare(){
+
+}
+gulp.task('distPrepare', distPrepare);
+
+/**
+ * lints files
+ */
+function lintFiles (){
+
+}
+gulp.task('jshint', lintFiles);
+
+/**
+ * lints and builds
+ */
+function distribute(){
+
+}
+gulp.task('dist', distribute);
